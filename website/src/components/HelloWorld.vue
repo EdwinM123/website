9ba040bff7b9d20ff2e95a5aa0a149e11b696631
Trible69 <edwinamunoz357@gmail.com>
2024-03-15 09:22:56 -0500

    vue!!!!!!

---
+++ website/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-info">info</button>
+    <button type="button" class="btn btn-warning">warning</button>
+  </div>
+</template>
+<script>
+export default{
+  name:'HelloWorld',
+  props:{
+    msg: String 
+  }
+}
+</script>
+<style scoped>
+</style>
